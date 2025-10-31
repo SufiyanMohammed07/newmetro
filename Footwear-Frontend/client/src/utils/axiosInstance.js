5d73698b3e2f0fca6bf9bd75d37a58c607edd3cd
SufiyanMohammed <sssufiyan98@gmail.com>
2025-10-31 17:33:50 +0530

    changed axiosinstance

--- Footwear-Frontend/client/src/utils/axiosInstance.js
+++ Footwear-Frontend/client/src/utils/axiosInstance.js
@@ -1,8 +1,15 @@
+// import axios from "axios";
+
+
+// const axiosInstance = axios.create({
+//   baseURL: `${import.meta.env.BASE_URL}`,
+// });
+
+// export default axiosInstance;
 import axios from "axios";
 
-
 const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.BASE_URL}`,
+  baseURL: import.meta.env.VITE_API_URL, // ✅ Correct one
 });
 
 export default axiosInstance;
