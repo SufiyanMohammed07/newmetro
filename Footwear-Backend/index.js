ba1f8f96b49be2e86a06b0269a75759e58f2c0a5
SufiyanMohammed <sssufiyan98@gmail.com>
2025-11-01 22:31:49 +0530

    image size increase to 10mb

--- Footwear-Backend/index.js
+++ Footwear-Backend/index.js
@@ -37,6 +37,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
