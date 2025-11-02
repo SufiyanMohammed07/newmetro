7f76bf9bfbae6e3506a42b470187f8c15fdae20b
SufiyanMohammed <sssufiyan98@gmail.com>
2025-11-02 10:31:46 +0530

    images limit to 10mb

--- Footwear-Backend/index.js
+++ Footwear-Backend/index.js
@@ -34,8 +34,11 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 
 
 const __filename = fileURLToPath(import.meta.url);
