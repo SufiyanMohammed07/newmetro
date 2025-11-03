67e95ed8e993a2335db5c63fb618f96e02e054dc
SufiyanMohammed <sssufiyan98@gmail.com>
2025-11-03 13:10:01 +0530

    error9

--- Footwear-Backend/models/Product.js
+++ Footwear-Backend/models/Product.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   category: { type: String, required: true },
-  subCategory: {type:String,required:true}, 
+  // subCategory: {type:String,required:true}, 
   price: { type: Number, required: true },
   description: { type: String },
   images: [{ type: String }],
